5225d71511fe23eca4bc52d95241a1ae15688bd0
ReimuA <thibault.dolley@epitech.eu>
2021-12-24 09:56:22 +0100

    Create autocomplete.sh

---
+++ autocomplete.sh
@@ -0,0 +1,21 @@
+_qb() 
+{
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts="--help --verbose --version"
+
+    filelist=""
+
+    for file in /home/dev/Workspace/* # Modify with your project directory
+    do
+        filename=${file##*/}
+        filelist="${filelist} ${filename}"
+    done
+
+        COMPREPLY=( $(compgen -W "${filelist}" -- ${cur}) )
+        return 0
+}
+
+complete -F _qb qb
