57ab72e1d34d486ebed47d87252bd6d7a3a7c3b0
Titouan CREACH <t.creach@unitee.io>
2021-12-24 11:08:32 +0000

    Automatically create a bin directory if not exist

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 npm ci
 
 sudo ln -sf $PWD/autocomplete.sh /etc/bash_completion.d/qb
+
+mkdir -p $HOME/bin >/dev/null 2>$1 # Create bin directory if it doesn't exist
+
 ln -sf $PWD/quickboot.js $HOME/bin/bootProject
 
 cat << 'EOF'
