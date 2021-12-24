ed8aff23c3cdc107d5681c3a8cac0ef7e88feb9a
ReimuA <thibault.dolley@epitech.eu>
2021-12-24 09:55:35 +0100

    Create qb.sh

---
+++ qb.sh
@@ -0,0 +1,11 @@
+
+qb() {
+  bootProject $1
+  cd /home/dev/Workspace/$1 # replace with your own working project directory.
+}
+
+
+# Uncomment if using .zshrc
+# autoload bashcompinit
+# bashcompinit
+# source /etc/bash_completion.d/qb
